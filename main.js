2be82433b383f419947129b8e48bbb7b65e8d3d1
Connor Ashlock <connor9804@yahoo.com>
2020-08-11 20:08:12 -0700

    query selector for header element and instantiated header class

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+var header = document.querySelector('HEADER')
 var table = document.querySelector('TABLE');
 var gradeTable = new GradeTable(table);
+var pageHeader = new PageHeader(header);
 var app = new App(gradeTable);
 
 app.start()
