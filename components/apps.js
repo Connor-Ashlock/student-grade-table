1c246a85bada57ef119d7e77daec6697b1a5986e
Connor Ashlock <connor9804@yahoo.com>
2020-08-12 15:04:35 -0700

    added 3 methods createGrade, handleCreateGradeError, and handleCreateGradeSuccess to the app class and bound them to the constructor

--- components/apps.js
+++ components/apps.js
@@ -5,6 +5,9 @@ class App {
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
     this.gradeForm = gradeForm;
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
   }
   handleGetGradesError(error){
     console.error(error);
@@ -29,6 +32,15 @@ class App {
     });
   }
   start(){
+    this.getGrades();
+  }
+  createGrade(name, course, grade) {
+    console.log(name, course, grade);
+  }
+  handleCreateGradeError(error) {
+    console.error(error);
+  }
+  handleCreateGradeSuccess() {
     this.getGrades();
   }
 }
