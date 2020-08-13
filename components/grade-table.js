faa13c113525865ed0312e3bbc4df56835905c4e
Connor Ashlock <connor9804@yahoo.com>
2020-08-12 17:46:21 -0700

    added method of renderGradeRow to GradeTable class

--- components/grade-table.js
+++ components/grade-table.js
@@ -23,4 +23,25 @@ class GradeTable {
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
   }
+  renderGradeRow(data, deleteGrade) {
+      var row = document.createElement('TR');
+      var stuName = document.createElement('TD');
+      var stuCourse = document.createElement('TD');
+      var stuGrade = document.createElement('TD');
+      var opsTd = document.createElement('TD');
+      var opsBtn = document.createElement('BUTTON');
+      stuName.textContent = data.name;
+      stuCourse.textContent = data.course;
+      stuGrade.textContent = data.grade;
+      stuName.classList = 'border-right-black';
+      stuCourse.classList = 'border-right-black';
+      stuGrade.classList = 'border-right-black';
+      opsBtn.classList = 'btn btn-danger';
+      opsBtn.textContent = 'Delete';
+      opsBtn.addEventListener('click', data.id);
+
+      opsTd.appendChild(opsBtn);
+      row.append(stuName, stuCourse, stuGrade, opsTd);
+      return row;
+  }
 }
